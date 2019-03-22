eda6f64a4cee93dac755ca8d732d659aee2b73ea
Dragos Vizireanu <viz.dragos@crossover.com>
2019-03-22 17:49:26 +0200

    Do simple health check operation - call a public rest api

--- src/main/java/com/pchealth/HealthCheckRunner.java
+++ src/main/java/com/pchealth/HealthCheckRunner.java
@@ -4,6 +4,13 @@ import com.google.common.util.concurrent.AbstractScheduledService;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import lombok.Data;
+import org.apache.http.HttpStatus;
 import sx.blah.discord.handle.obj.IChannel;
 
 public class HealthCheckRunner extends AbstractScheduledService {
@@ -18,9 +25,21 @@ public class HealthCheckRunner extends AbstractScheduledService {
         this.discordChannel = discordChannel;
     }
 
+    public static synchronized HealthCheckRunner getInstance() {
+        if (instance == null) {
+            instance = new HealthCheckRunner();
+        }
+        return instance;
+    }
+
     @Override
     protected void runOneIteration() throws Exception {
-        BotUtils.sendMessage(this.discordChannel , "PC OK ! [" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "]");
+        if (canDoOperation()) {
+            BotUtils.sendMessage(this.discordChannel,
+                    "PC OK ! [" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "]");
+        } else {
+            System.out.println("FATAL - PC can not do simple operation !!!");
+        }
     }
 
     @Override
@@ -28,10 +47,26 @@ public class HealthCheckRunner extends AbstractScheduledService {
         return Scheduler.newFixedRateSchedule(0, 3, TimeUnit.MINUTES);
     }
 
-    public static synchronized HealthCheckRunner getInstance() {
-        if (instance == null) {
-            instance = new HealthCheckRunner();
-        }
-        return instance;
+    private boolean canDoOperation() {
+        // simple health check - ping some public www rest api - https://rest.ensembl.org/documentation/info/ping
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("https://rest.ensembl.org/");
+        WebTarget pingWebTarget = webTarget.path("info/ping");
+
+        Response response = pingWebTarget.request(MediaType.APPLICATION_JSON).get();
+        Ping ping = response.readEntity(Ping.class);
+
+        return HttpStatus.SC_OK == response.getStatus() && ping.isOK();
+    }
+
+}
+
+@Data
+class Ping {
+
+    int ping;
+
+    public boolean isOK() {
+        return ping == 1;
     }
 }
